a3cb1c00a9fd0d57ea053d4c9c8f6d926474e1e2
Athos <athosbransk@gmail.com.br>
2022-08-15 23:56:03 -0300

    banco atualizado

--- ScriptStudyCards.sql
+++ ScriptStudyCards.sql
@@ -2,4 +2,9 @@ create user 'athos' identified by '2003';
 create database studycards;
 grant all on studycards.* to athos;
 
- 
+ create table carta(
+ id int auto_increment not null primary key,
+ titulo varchar(32) not null,
+ frente varchar(360) not null,
+ verso varchar(360) not null
+ );
